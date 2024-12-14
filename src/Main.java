4f8935c95ef0b0aa2ca8c3cf72eb12c9623565c5
josshare <36396116+josshare@users.noreply.github.com>
2024-12-13 19:44:23 -0600

    Fix: Funcionado primer ejercicio.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,3 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         Cliente cliente1 = new Cliente("Cliente 1", new int[]{2, 2, 1, 5, 2, 3});
